1ac5420917aebb54aafee1b1cd1edbf99b1379ed
OKKyu <kyukyuinskt@gmail.com>
2021-01-13 09:16:53 +0900

    setup.shを再実行した場合の対応
    古いvenvは削除する。

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-
-if [ ! -d ./venv ]; then
-  mkdir venv
-  python3 -m venv venv
+if [ -d ./venv ]; then
+  rm -rf ./venv/.*
+  rm -rf ./venv/*
+  rmdir ./venv
 fi
-
+mkdir venv
+python3 -m venv venv
 source ./venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
